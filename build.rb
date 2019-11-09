5fd8ad994d85ef7020e3fbc49d53b62265f7dd1e
David Bengoa <david@bengoarocandio.com>
2019-11-09 10:57:32 +0100

    Pin dependencies

--- build.rb
+++ build.rb
@@ -2,28 +2,25 @@ require 'digest'
 system('rm -f -r ./warpwallet ./pkg')
 
 expected_pkgs = {
-  #'9ba54cc4b69478b691526ec5b972b069' => 'src/github.com/vsergeev/btckeygenie/btckey/elliptic.go',
-  #'9e3f960f49c8f31044fad08335aa2dba' => 'src/github.com/vsergeev/btckeygenie/btckey/btckey.go',
-
-  '06005f21dadaa2938c2069e7521f52f5' => 'src/golang.org/x/crypto/scrypt/scrypt.go',
-  '416c534c652727476f6d545a599601ce' => 'src/golang.org/x/crypto/ripemd160/ripemd160.go',
-  '0d36148ce6c0821409a0be2d7bfde4b0' => 'src/golang.org/x/crypto/ripemd160/ripemd160block.go',
-  'f9d28bcecfb4a553f6bc7abe724ceadd' => 'src/golang.org/x/crypto/pbkdf2/pbkdf2.go',
-
   'ce76f423d92e7b64b81dc15dc78c9b2b' => 'src/github.com/ThePiachu/Go/mymath/bitelliptic/bitelliptic.go',
   'a20222f19fae8a66ff60ef0c2ec36b0c' => 'src/github.com/ThePiachu/Go/mymath/bitecdsa/bitecdsa.go',
   '26fd760cc7aee1b25668fbf6d4f42b35' => 'src/github.com/jbenet/go-base58/base58.go',
 
-  '3e9787f8fcaa0f1e4b5387f9eb5533af' => 'src/golang.org/x/crypto/blake2b/blake2b.go',
-  '01817309b90c01500ee15127a498cd4c' => 'src/golang.org/x/crypto/blake2b/blake2b_ref.go',
-  '748a7ff139259328aac9776add98d449' => 'src/golang.org/x/crypto/blake2b/blake2b_generic.go',
+  'abf1d791399ffd9ff4483e847998a799' => 'src/golang.org/x/crypto/scrypt/scrypt.go',
+  '0aa027a87deaad179e079d88b0df91be' => 'src/golang.org/x/crypto/ripemd160/ripemd160.go',
+  '4587c98398c66cc06ddbc34b3bb3bf57' => 'src/golang.org/x/crypto/ripemd160/ripemd160block.go',
+  'f9d28bcecfb4a553f6bc7abe724ceadd' => 'src/golang.org/x/crypto/pbkdf2/pbkdf2.go',
 
-  '77e38ecf7567eec42ca73a6f919b6b3d' => 'src/github.com/ethereum/go-ethereum/crypto/sha3/hashes.go',
-  'ce6b72f18ca816dd52721347eaba0402' => 'src/github.com/ethereum/go-ethereum/crypto/sha3/keccakf_amd64.go',
-  '246bdf5354a51a03d0df2059ab4b9bc2' => 'src/github.com/ethereum/go-ethereum/crypto/sha3/keccakf_amd64.s',
-  '5bce29d1ffcf1517c799af1c85dc238d' => 'src/github.com/ethereum/go-ethereum/crypto/sha3/sha3.go',
-  'e6e13b0bc0f693d9d10e8fae1f9687b4' => 'src/github.com/ethereum/go-ethereum/crypto/sha3/xor_unaligned.go',
+  '3bc3733c2162874f6451adbca1c4dd52' => 'src/golang.org/x/crypto/blake2b/blake2b.go',
+  'df7eab6ad6cd40e5e48f6266e38504d2' => 'src/golang.org/x/crypto/blake2b/blake2b_generic.go',
+  '4eaddf8cdcc42e3e3c1a2c5fe72adf14' => 'src/golang.org/x/crypto/blake2b/blake2b_amd64.go',
 
+  'bc4095bb30b736729da43ac9b6074567' => 'src/golang.org/x/crypto/sha3/hashes.go',
+  '58cc59aa11377c1fe5340650551c9087' => 'src/golang.org/x/crypto/sha3/hashes_generic.go',
+  '58fac3b5ca71d8eab3c334a65e992497' => 'src/golang.org/x/crypto/sha3/keccakf_amd64.go',
+  '246bdf5354a51a03d0df2059ab4b9bc2' => 'src/golang.org/x/crypto/sha3/keccakf_amd64.s',
+  'f26149e9bce504a2a05a183224ea0d54' => 'src/golang.org/x/crypto/sha3/sha3.go',
+  '86af78583c20719dac4ad9444722dd4b' => 'src/golang.org/x/crypto/sha3/xor_unaligned.go'
 }.invert
 
 actual_pkgs = `find src -type f`.split("\n")
